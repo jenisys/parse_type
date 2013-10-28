691ed02888ec30f75b43d7f9e2acd2bd2c4584bd
jenisys <jens_engel@web.de>
2013-10-28 19:33:29 +0100

    Add travis CI build script.

--- setup.py
+++ setup.py
@@ -109,6 +109,8 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Code Generators",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: BSD License",
