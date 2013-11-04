bd940f74275e37680284fe2fb044d12953b68ffb
jenisys <jens_engel@web.de>
2013-11-05 00:10:17 +0100

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ if USE_PYTEST_RUNNER:
 # -----------------------------------------------------------------------------
 setup(
     name = "parse_type",
-    version = "0.3.3",
+    version = "0.3.4",
     author = "Jens Engel",
     author_email = "jens_engel@nowhere.xxx",
     url = "https://github.com/jenisys/parse_type",
