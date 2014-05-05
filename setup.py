e9093d3761a8ce1c07e94f338cff60624e3232b9
jenisys <jenisys@users.noreply.github.com>
2014-05-05 22:25:35 +0200

    UPDATE, support Python 3.4 now, too.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ extra = dict(
     install_requires = requirements,
     tests_require = [],
     extras_require = {
-        'docs':    ["sphinx>=1.1"],
+        'docs':    ["sphinx>=1.2"],
         'develop': [
             "coverage", "pytest", "pytest-cov",
             "pytest-runner",
@@ -109,6 +109,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Code Generators",
